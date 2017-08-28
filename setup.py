67266b5bb21dd92e7def0bdc41adddb2584c1868
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-08-28 21:40:19 +0300

    0.1.1 release: add meta and README to pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
+import os
+
 from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
     name='rl_wsd_labeled',
-    version='0.1.0',
+    version='0.1.1',
+    license='MIT',
     packages=['rl_wsd_labeled'],
+    url='https://github.com/lopuhin/ruslang-wsd-labeled/',
+    include_package_data=True,
+    author='Konstantin Lopuhin',
+    author_email='kostia.lopuhin@gmail.com',
+    description='Labeled contexts of Russian polysemous words',
+    long_description=read('README.rst'),
     zip_safe=False,
     package_data={
         'rl_wsd_labeled': [
